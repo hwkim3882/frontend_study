653b6f8ff26a153a657e6d3a18e0c4382028b67c
hwkim3882 <kimhyowon1@gmail.com>
2021-02-02 18:14:10 +0900

    upadate

---
+++ clone_projects/github_clone/main.js
@@ -0,0 +1,6 @@
+(function (window, document){
+    'use strict';
+    
+    const $toggles = document.querySelector('.toggle');
+    const $toggleBtn = document.getElementById('')
+})(window, document)
